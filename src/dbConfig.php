6759159785be09bc8cc40ef383d993cf929ee7a1
klikar <klikarmarek@gmail.com>
2017-06-10 11:41:22 +0200

    reorganize dbConfig

--- src/dbConfig.php
+++ src/dbConfig.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'coderslab');
-define('DB_DB', 'twitter_mk');
+defined('DB_HOST') ? NULL : define('DB_HOST', 'localhost');
+defined('DB_USER') ? NULL : define('DB_USER', 'root');
+defined('DB_PASS') ? NULL : define('DB_PASS', 'coderslab');
+defined('DB_DB') ? NULL : define('DB_DB', 'twitter_mk');
